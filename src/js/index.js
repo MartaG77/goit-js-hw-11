cb7f03a82108e00a8e0e05601671948a22ecd605
MartaG77 <129276573+MartaG77@users.noreply.github.com>
2023-10-15 19:10:59 +0200

    update

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import {fetchImages} from './pixabay-api';
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+loadMore.hidden = true;
+let currentPage = 1;
+let questionValue = '';
+const showGallery = images => {
+  images.map(image => {
+    gallery.insertAdjacentHTML(
+      'beforeend',
+      `<div class='photo-card'><a class='photo-card--link' href='${image.largeImageURL}'><img src='${image.webformatURL}' alt='${image.tags}' loading='lazy'/></a><div class='info'><p class='info-item'><b>Likes</b>${image.likes}</p><p class='info-item'><b>Views</b>${image.views}</p><p class='info-item'><b>Comments</b>${image.comments}</p><p class='info-item'><b>Downloads</b>${image.downloads}</p></div></div>`
+    );
+  });
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+};
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputText = event.currentTarget.elements.searchQuery.value;
+  if (inputText.trim() === '') {
+    Notiflix.Report.failure('Error. You must enter something.');
+    questionValue = '';
+    return;
+  }
+  questionValue = inputText;
+  gallery.innerHTML = '';
+  currentPage = 1;
+  fetchImages();
+});
+loadMore.addEventListener('click', () => {
+  currentPage++;
+  fetchImages();
+  const image = document.querySelector('.gallery');
+  if (gallery.firstElementChild) {
+    const {height: cardHeight} =
+      image.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+});
+export {showGallery, currentPage, questionValue, loadMore};
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,43 @@
-
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import axios from 'axios';
+import {showGallery, currentPage, loadMore, questionValue} from '.';
+const URL = 'https://pixabay.com/api/?key=';
+const fetchImages = async () => {
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        key: '40040363-d145279656575dd2a7df2e0d0',
+        q: questionValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: currentPage,
+        per_page: 40,
+      },
+    });
+    if (response.data.hits.length === 0) {
+      Notiflix.Report.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      ); loadMore.disabled = true;
+        } else {
+      showGallery(response.data.hits);
+      if (currentPage === 1) {
+        Notiflix.Report.success(
+          `Hooray! We found ${response.data.totalHits} images.`
+        );
+      }
+      if (response.data.totalHits <= currentPage * 40) {
+        loadMore.disabled = true;
+        Notiflix.Report.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        loadMore.disabled = false;
+        loadMore.style.display = 'block';
+      }
+    }
+  } catch (error) {
+  }
+};
+export {fetchImages};
